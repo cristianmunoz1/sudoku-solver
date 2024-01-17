074bcc82b4575e2fba86844ccc877c5c186bfe49
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2024-01-17 13:24:56 -0500

    Se agrega la lógica para verificar por cuadrantes

--- src/components/SudokuGridGenerator.js
+++ src/components/SudokuGridGenerator.js
@@ -4,7 +4,7 @@
         let size = matrix[0].length;
         //Array que contiene en principio todos los valores posibles que puede tener el sudoku
         let validNumbers = [];
-        for(let i = 1; i<=size; i++){
+        for(let i = 1; i<=9; i++){
             validNumbers.push(i);
         }
 
@@ -14,7 +14,7 @@
             const actualValue = matrix[positionX][j];
             if(actualValue !== 0){
                 const indexToDelete = validNumbers.indexOf(actualValue);
-                validNumbers.splice(indexToDelete, 1);
+                validNumbers.splice(indexToDelete, 1); 
             }
         }
 
@@ -27,6 +27,24 @@
             }
         }
         
+        //Encontramos la primera fila y columna del cuadrante en el que nos encontramos según la posición
+        let firstQuadrantRow = Math.floor(positionX/3)*3
+        let lastQuadrantRow = firstQuadrantRow + 2;
+
+        let firstQuadrantColumn = Math.floor(positionY/3)*3
+        let lastQuadrantColumn = firstQuadrantColumn + 2;
+        
+        //Con este ciclo nos aseguramos que en validNumbers no haya números que estén en el mismo cuadrante
+/*         for(let i = firstQuadrantRow; i<=lastQuadrantRow; i++){
+            for(let j = firstQuadrantColumn; j <= lastQuadrantColumn; j++){
+                let actualValue = matrix[i][j];
+                
+                if(actualValue !== 0){
+                    let index = validNumbers.indexOf(actualValue);
+                    validNumbers.splice(index, 1);
+                }
+            }
+        } */
 
         return validNumbers;
     }
